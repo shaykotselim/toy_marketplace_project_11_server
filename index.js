293200d915a80ebf08e9a29c58274a86a5bd3f5a
shaykot43500 <hossainshaykot@gmail.com>
2023-05-21 12:01:19 +0600

    added updateDoc

--- index.js
+++ index.js
@@ -58,9 +58,15 @@ async function run() {
     // Patch Area here ......................................................................
     app.patch('/products/:id', async(req, res)=>{
         const id = req.params.id;
-        const filter = {_id: new ObjectId(id)}
+        const filter = {_id: new ObjectId(id) };
         const updatedProduct = req.body;
         console.log(updatedProduct);
+
+        const updateDoc = {
+            $set: {
+                status: updatedProduct.status
+            },
+        };
 
 
     })
