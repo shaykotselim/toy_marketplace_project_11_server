e05dddc06ef68a167165eab7a0d298d22a03a1c2
shaykot43500 <hossainshaykot@gmail.com>
2023-05-21 09:03:46 +0600

    added new cmnt

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-// middleware
+//middleware area here--------------------------------------------------------------------
 
 app.use(cors());
 app.use(express.json());
 
 
-/// URI Connection 
+//URI Connection--------------------------------------------------------------------------
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.klglhlk.mongodb.net/?retryWrites=true&w=majority`
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version-----
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -27,13 +27,14 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     
 
     const productCollection = client.db('Category').collection('products');
     
-    //Get single data here..........................................................
+    //Get single data here.................................................................
     app.get('/products', async (req, res)=>{
         console.log(req.query.email);
         let query = {};
@@ -44,24 +45,24 @@ async function run() {
         res.send(result)
     })
     
-    //Post Data Here ............................................................... 
+    //Post Data Here ....................................................................... 
     app.post('/products',async(req, res) =>{
         const product = req.body;
         console.log(product);
         
-        //insert data here..........................................
+    //insert area data here..................................................................
         const result = await productCollection.insertOne(product);
         res.send(result )
 
     })
-    // Patch Area
+    // Patch Area here ......................................................................
     app.patch('/products/:id', async(req, res)=>{
         const updatedProduct = req.body;
         console.log(updatedProduct);
 
     })
 
-    //delete
+    //delete from ui data area here..........................................................
     app.delete('/products/:id', async(req, res)=>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
@@ -69,14 +70,14 @@ async function run() {
         res.send(result)
     })
     
-    //Getting Data Here ............................................................
+    //Getting Data Here ....................................................................
     app.get('/products', async(req, res)=>{
             const cursor = productCollection.find();
             const result = await cursor.toArray();
             res.send(result);
     })
 
-    // Send a ping to confirm a successful connection
+    // Send a ping to confirm a successful connection.......................................
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
