413b6dd0919dbf1eaf8e2e36b68a80a61235a09d
shaykot43500 <hossainshaykot@gmail.com>
2023-05-21 11:54:54 +0600

    added patch filter

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function run() {
     // Patch Area here ......................................................................
     app.patch('/products/:id', async(req, res)=>{
         const id = req.params.id;
-    
+        const filter = {_id: new ObjectId(id)}
         const updatedProduct = req.body;
         console.log(updatedProduct);
 
